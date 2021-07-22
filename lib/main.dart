cc21889805a46e9ec24ffb933b72d3fee19f74a3
Andy <karandriy@gmail.com>
2021-07-22 12:20:34 +0300

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+
+int randomInteger(int max) {
+  Random random = new Random();
+
+  return random.nextInt(max) + 1;
+}
 
 void main() {
   return runApp(
@@ -18,6 +25,50 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Dices(),
+    );
+  }
+}
+
+class Dices extends StatefulWidget {
+  @override
+  _DicesState createState() => _DicesState();
+}
+
+class _DicesState extends State<Dices> {
+  int firstDice = randomInteger(6);
+  int secondDice = randomInteger(6);
+
+  void _changeDices() {
+    setState(() {
+      firstDice = randomInteger(6);
+      secondDice = randomInteger(6);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _changeDices,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Image.asset('images/dice$firstDice.png'),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Image.asset('images/dice$secondDice.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
